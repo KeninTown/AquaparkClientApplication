4f22840fe0d9cac86602fb7b9b68127e3562cf7c
Alexander <soldatovalex207z@gmail.com>
2023-05-11 16:53:51 +0300

    finished

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const {allData, clientsBetweenDates, getInfo} = require('./sequilese.js');
+const {allData, clientsBetweenDates, getInfo, addClient} = require('./sequilese.js');
 
 const app = express();
 
@@ -9,6 +9,7 @@ const app = express();
 app.use('/',express.static(__dirname + '/public'));
 app.use('/api/user', express.static(__dirname + '/public'));
 app.use('/api/admin', express.static(__dirname + '/public'));
+app.use('/api/:role/:method', express.static(__dirname + '/public'));
 app.use('/api/admin/:method', express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -35,10 +36,15 @@ app.get('/api/:role', (req, res) => {
 
 app.route('/api/:role/create')
   .get((req, res) => {
-    res.render(__dirname +'/templates/createNewClient.ejs')
+    res.render(__dirname +'/templates/createNewClient.ejs', {succes:''})
   })
-  .post((req, res) => {
-    
+  .post(async (req, res) => {
+    try {
+      await addClient(req.body.fullName, req.body.dateOfBirth, req.body.discount);
+      res.render(__dirname +'/templates/createNewClient.ejs', {succes:'Клиент успешно создан и добавлен в базу данных'})
+    } catch (error) {
+      console.log(error);
+    }
 })
 
 app.get('/api/admin/amount', (req, res) => {
@@ -46,72 +52,24 @@ app.get('/api/admin/amount', (req, res) => {
 })
 
 app.get('/api/admin/amount/data', async (req, res)  =>{
-  const data = await clientsBetweenDates(req.query.from, req.query.to);
-  res.render(__dirname +'/templates/adminFullData.ejs', {users:data});
+  try {
+    const data = await clientsBetweenDates(req.query.from, req.query.to);
+    res.render(__dirname +'/templates/adminFullData.ejs', {users:data});
+  } catch (error) {
+    console.log(error);
+  }
+  
 })
 
 app.get('/api/admin/full', async (req, res) => {
-  const data = await allData();
-  console.log(data);
-  res.render(__dirname +'/templates/adminFullData.ejs', {users:data});
-})
-
-
-
-
-// Connection to DB
-
-
-// const executeSQL = (sql, callback) => {
-//   let connection = new Connection({
-//     "authentication": {
-//       "options": {
-//         "userName": "sa",
-//         "password": "123"
-//       },
-//       "type": "default"
-//     },
-//     "server": "KeningTown",
-//     "options": {
-//       "validateBulkLoadParameters": false,
-//       "rowCollectionOnRequestCompletion": true,
-//       "database": "aquapark",
-//       "encrypt": false
-//     }
-//   });
-
-// connection.connect((err) => {
-//   if (err)
-//     return callback(err, null);
-//   const request = new Request(sql, (err, rowCount, rows) => {
-//     connection.close();
-//     if (err)
-//       return callback(err, null);
-//     callback(null, {rowCount, rows});
-//   });
-
-//   request.on('row', columns => {
-//     columns.forEach(column => {
-//     console.log(column.value);
-//     });
-//   });
   
-//   connection.execSql(request);
-// });
-// };
-
-// executeSQL("SELECT zone_name FROM Zones", (err, data) => {
-//   if (err)
-//     console.error(err);
-//   console.log(data.rowCount);
-// });
-
-// executeSQL("SELECT * FROM Client", (err, data) => {
-//   if (err)
-//     console.error(err);
-//   console.log(data.rowCount);
-// });
-
+  try {
+    const data = await allData();
+    res.render(__dirname +'/templates/adminFullData.ejs', {users:data});
+  } catch (error) {
+    console.log(error);
+  }
+})
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`)
 })
--- sequilese.js
+++ sequilese.js
@@ -2,21 +2,18 @@ const Sequelize = require('sequelize').Sequelize;
 const DataTypes = require('sequelize').DataTypes;
 const sequelize = new Sequelize('aquapark', 'sa', '123', {
     host: 'KeningTown',
-    dialect: 'mssql'
+    dialect: 'mssql',
+    define: {
+        freezeTableName: true
+    },
+    dialectOptions: {
+        options: {
+          encrypt: false // для подключения к базе данных с использованием шифрования SSL
+        }
+      }
   });
 
-let getUsers = async () =>{
-    try {
-        await sequelize.authenticate();
-        console.log('Соединение открыто');
-        const users = await sequelize.query('SELECT * FROM Client', { type: Sequelize.QueryTypes.SELECT });
-        await sequelize.close();
-        console.log('Соединение закрыто');
-        console.log(users[0]);
-    } catch (error) {
-        console.log(error);
-    }
-}
+
 //Получить все данные
 const allData = async () => {
     try {
@@ -30,9 +27,6 @@ const allData = async () => {
 
 const clientsBetweenDates = async (from, to) => {
     try {
-        // let regEx = /-/gi
-        // let updatedFrom = from.replace(regEx, '.')
-        // let updatedTo = to.replace(regEx, '.')
         await sequelize.authenticate();
 
         const rawData = await allData();
@@ -50,8 +44,6 @@ const clientsBetweenDates = async (from, to) => {
             if( clientStartTime > dateFrom && clientEndTime < dateTo)
                 data.push(client)
         })
-        // const query = `SELECT  full_name, date_of_birth, discount_percent, zone_name, start_time, end_time from Zone_visits join Bracelets on bracelet_id = visited_bracelet_id join Client on client_id = client_id_on_bracelet join Zones on zone_id = visited_zone_id where start_time > \'${updatedFrom}\' and end_time < \'${updatedTo}\'`;
-        // const data = await sequelize.query(query, {type: Sequelize.QueryTypes.SELECT });
         return data;
     } catch (error) {
       console.log(error)  
@@ -71,92 +63,48 @@ const getInfo = async (fullName) =>{
         })
         return data;
     } catch (error) {
-        console.log(errro);
-    }
-}
-
-// console.log(clientsBetweenDates('13-09-2000', '13-09-2025'));
-module.exports = {clientsBetweenDates, allData, getInfo};
-
-
-
-
-
-
-// Создать клиента
-const createClient = async () =>{
-    try {
-        await sequelize.authenticate();
-        const data = await sequelize.query('insert into Client (full_name, date_of_birth, discount_percent) values (\'Солдатов Алексей Александрович\', \'24.04.2003\', 3)');
-        await sequelize.close();
-        console.log(data);
-
-    } catch (error) {
         console.log(error);
     }
 }
 
-// createClient();
-
-let getId = async () =>{
-    try {
-        await sequelize.authenticate();
-        console.log('Соединение открыто');
-        const users = await sequelize.query('SELECT * FROM Table_1', { type: Sequelize.QueryTypes.SELECT });
-        // await sequelize.close();
-        console.log('Соединение закрыто');
-        console.log(users);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const User = sequelize.define('user', {
-    id: {
-      type: DataTypes.NUMBER,
+const Client = sequelize.define('Client', {
+    client_id: {
+      type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey:  true
+      primaryKey:  true,
+      autoIncrement: true
     },
-    name: {
+    full_name: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
-  });
+      allowNull: true
+    },
+    date_of_birth:{
+        type: DataTypes.DATE,
+        allowNull:true
+    },
+    discount_percent: DataTypes.INTEGER
+  }, {timestamps:false});
 
-const existingTable = sequelize.define('Table_1', {}, { freezeTableName: true });
 
-async function syncModels() {
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-
-    // Передаём имя существующей таблицы во втором аргументе метода define
-    await User.init(existingTable.rawAttributes, { sequelize, modelName: 'user' });
-    console.log('Models have been synchronized successfully.');
-    // await sequelize.close();
-    
-
-    // console.log('Ваня добавлен')
+const addClient = async (fullName, date, discount) =>{
+    try {
+        const dateArr = date.split('.')
+        await Client.create({
+            full_name: fullName,
+            date_of_birth: new Date(`${dateArr[2]}-${dateArr[1]}-${dateArr[0]}`),
+            discount_percent: discount
+        })
+        return true; 
     } catch (error) {
-    console.error('Unable to connect to the database:', error);
+        console.log(error)
     }
 }
 
-async function addUser() {
-    try {
-      await sequelize.authenticate(); // Проверяем соединение
-      console.log('Connection has been established successfully.');
-  
-      // Добавление записи в базу данных
-      await User.create({id: 232, name: 'Doe'});
-      console.log('Record has been added successfully.');
-  
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-    }
-  }
+module.exports = {clientsBetweenDates, allData, getInfo, addClient};
 
-// syncModels();
 
-// addUser();
+
+
+
+
 //SELECT start_time, end_time, full_name. zone_name from Zone_visits join Bracelets on bracelet_id = visited_bracelet_id join Client on client_id = client_id_on_bracelet join Zones on zone_id = visited_zone_is
